10030526e3c93c10d2fc80b6457bc37cc93619c7
Steven G. <estengrove99@gmail.com>
2025-10-04 10:03:37 -0700

    FIX: Updated SASS Partials declarations for modern syntax.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,21 @@ export default defineConfig({
 	server: {
 		host: true,
 		port: 5175,
+	},
+	resolve: {
+		alias: {
+			"@sass": path.resolve(__dirname, "src/sass"),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+          @use "@sass/variables" as *;
+          @use "@sass/mixins" as *;
+          @use "@sass/custom" as *;
+        `,
+			},
+		},
 	},
 });
